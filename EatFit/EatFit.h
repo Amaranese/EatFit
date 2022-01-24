f75989991058ed0d586c26e72181dea29f478823
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-24 11:18:08 +0100

    Create EatFit.h

---
+++ EatFit/EatFit.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for EatFit.
+FOUNDATION_EXPORT double EatFitVersionNumber;
+
+//! Project version string for EatFit.
+FOUNDATION_EXPORT const unsigned char EatFitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EatFit/PublicHeader.h>
+
+
