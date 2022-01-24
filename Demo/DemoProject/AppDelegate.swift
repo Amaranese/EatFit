4b9a40e60de71450cff234ed433fec6fcb9d49b3
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-24 11:17:26 +0100

    Create AppDelegate.swift

---
+++ Demo/DemoProject/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  EatFit Demo Project
+//
+//  Created by aleksey on 31.08.15.
+//  Copyright (c) 2015 aleksey chernish. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
